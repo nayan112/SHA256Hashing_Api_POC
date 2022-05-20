034917778a129db30a5a881e000695752ac12bdb
Nayan Bhagowaty <nayan.bhagowaty@gmail.com>
2022-05-20 11:10:28 +0530

    Add project files.

---
+++ SHA256Hashing_Api_POC/Controllers/CryptoController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading;
+
+namespace SHA256Hashing_Api_POC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HashingController : ControllerBase
+    {
+        private readonly ILogger<HashingController> _logger;
+
+        public HashingController(ILogger<HashingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("GetHash")]
+        public async Task<ActionResult<string>> GetHash(string text)
+        {
+            var hash = await GetHashSha256(text);
+            return hash;
+        }
+
+        [HttpPost("GetFileHash")]
+        public async Task<ActionResult<string>> GetFileHash(IFormFile file)
+        {
+            //validate file name, type, ext
+            //exception handling
+            string hash = String.Empty;
+            if (file.Length > 0)
+            {
+                try
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(stream);
+                        hash = await GetHashSha256(Encoding.UTF8.GetString(stream.GetBuffer(), 0, (int)stream.Length));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return hash;
+        }
+
+        private static async Task<string> GetHashSha256(string text)
+        {
+            string hashString = string.Empty;
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            using (var myHash = new SHA256Managed())
+            {
+                byte[] hash = myHash.ComputeHash(bytes);
+                foreach (byte x in hash)
+                {
+                    hashString += String.Format("{0:x2}", x);
+                }
+            }
+            return hashString;
+        }
+    }
+}
---
+++ SHA256Hashing_Api_POC/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SHA256Hashing_Api_POC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => {
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "File Hashing");
+            });
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
